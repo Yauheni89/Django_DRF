c6a1667ec9628b7d798619581dc1585036d9c0dd
Yauheni Lampe <spakv49@gmail.com>
2025-01-27 21:19:38 +0500

    Дополнение проекта

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "courses",
     "users",
+
     "rest_framework",
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +72,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
 
 DATABASES = {
     "default": {
